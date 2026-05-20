2ab8151a82b76839a0cd43eee4f252a750473449
Alex <alexandraiporre@gmail.com>
2026-05-20 17:25:31 -0400

    vuelta a railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://parqueo-backend-tau.vercel.app/api/v1',
-  socketUrl: 'https://parqueo-backend-tau.vercel.app'
+  apiUrl: 'https://parqueo-backend-production.up.railway.app/api/v1',
+  socketUrl: 'https://parqueo-backend-production.up.railway.app'
+  // apiUrl: 'https://parqueo-backend-tau.vercel.app/api/v1',
+  // socketUrl: 'https://parqueo-backend-tau.vercel.app'
 };
