e850307f46a4b60667ea2fa9dea8c88aea2e873c
Alex <alexandraiporre@gmail.com>
2026-05-20 16:44:55 -0400

    nuevo backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://parqueo-backend-production.up.railway.app/api/v1'
+  apiUrl: 'https://parqueo-backend-tau.vercel.app/api/v1',
+  socketUrl: 'https://parqueo-backend-tau.vercel.app'
 };
